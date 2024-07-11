d13136e0a65f0292d4942f4ff2377cc31103de12
Armen <armen_grigoryan_101@list.ru>
2024-07-11 16:53:13 +0700

    Deleted

--- Project 5/dz/dz 2/JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-  let width = document.documentElement.clientWidth;
-  let height = document.documentElement.clientHeight;
-  alert(`Ширина ${width} 
-Высота ${height}`);
-});
