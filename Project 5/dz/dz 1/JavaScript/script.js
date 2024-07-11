c888fcc0453d6715465db37826050af56a2f6395
Armen <armen_grigoryan_101@list.ru>
2024-07-11 17:02:04 +0700

    dfg

--- Project 5/dz/dz 1/JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-const iconOne = document.querySelector('.icon-one')
-const iconTwo = document.querySelector('.icon-two');
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-  iconOne.classList.toggle('icon-one-magic');
-  iconTwo.classList.toggle('icon-two-magic');
-});
